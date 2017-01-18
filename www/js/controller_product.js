73b9ed5a6a524fec9fe61ae2f1d534777e989c56
MikeElsers <MikeElsers@web.de>
2017-01-18 11:20:04 +0100

    - added help information to filter page  (for that:  w3  accordion   component used)   - helper texts are contained in the  controller_product.js  file
    - removed the filter term summary (commented, still in source code)
     Changes to be committed:
    	modified:   www/css/mystyles.css
    	new file:   www/css/w3.css
    	modified:   www/index.html
    	modified:   www/js/controller_product.js
    	modified:   www/templates/filter.html
    	modified:   www/templates/productlist.html

--- www/js/controller_product.js
+++ www/js/controller_product.js
@@ -58,7 +58,43 @@ angular.module('starter')
       { producer: 'SimpleThings', id: 6, product_name_short:'simpleThing01', image:'img/prod/6.jpg' }
     ];
     */
-    
+  
+    //---------------  FILTER EXPLANATIONS --------------------
+              
+        $scope.toggleVisibility = function (id) {
+            
+                //console.log (" myfunction found !");
+                var x = document.getElementById(id);
+            
+                if (x.className.indexOf("w3-show") == -1) {
+                        x.className += " w3-show";
+                } else { 
+                        x.className = x.className.replace(" w3-show", "");
+                }
+        }             
+                
+                
+      $scope.filterComments =[
+/**/    
+        {name:"General usage", 
+         comment:'Type in characters (without quotes,  case-INsensitive) to filter for those terms; Example:  [   tool   ] -> keeps only those products that contain the term tool anywhere in all parts.'
+        } ,
+        {
+            name:"Negating expressions",
+            comment:" Negation of a term:  -> [    ! tool  ]  ->  keeps only those products that do NOT contain  'tool'." 
+        },
+        {
+            name:"Quick filter",
+            comment:" ... also named: Filter 1;  ... is the same filter as on the top of the  productList page.  Filter 2 and 3 are accessible only here !" 
+        },
+        {
+            name:"Setting multiple filters",
+            comment:"All filters are chained by 'AND'" 
+        } 
+      ];
+          
+      //-------------------------------------------------
+                
 }]
 )
 
